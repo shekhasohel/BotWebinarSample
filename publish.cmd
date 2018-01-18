caf5d55c6ab83ef8fbbb60494edbd8369a1d92ef
Sohel Shekha <shekha.sohel@gmail.com>
2018-01-18 10:03:26 +0800

    Initial Commit
    Initial Code

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.LuisBot.sln -p:DeployOnBuild=true -p:PublishProfile=tumtum-Web-Deploy.pubxml -p:Password=B4pXQu3dh4TiPo3jg27ecDhAuANoadqaDsW9Dm4laqlAMiAboDWGpgeci3ac
+
